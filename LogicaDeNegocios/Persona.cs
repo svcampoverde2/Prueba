79d76fc2343c3df06db818259d95023fc1fe98d4
SERGIO VICENTE CAMPOVERDE LEIVA <sergio.compoverdel@ug.edu.ec>
2022-02-16 18:28:13 -0500

    sexo douyble

--- LogicaDeNegocios/Persona.cs
+++ LogicaDeNegocios/Persona.cs
@@ -8,9 +8,8 @@ namespace LogicaDeNegocios
     {
         private int cedula;
         private string nombre;
-        private string sexo;
-        private int telefono;
-        private int edad;
+        private double sexo;
+        private string telefono;
         /// <exclude />
         public Persona() { }
 
@@ -22,38 +21,27 @@ namespace LogicaDeNegocios
         /// <param name="sexo"></param>
         /// <param name="telefono"></param>
         
-        public Persona(int cedula, string nombre, string sexo, string telefono, int edad)
-      
+        public Persona(int cedula, string nombre, double sexo, string telefono)
         {
             this.cedula = cedula;
             this.nombre = nombre;
             this.sexo = sexo;
             this.telefono = telefono;
-            this.edad= edad;
 
         }
         //se realiza comentario
         
-       
-        /// <summary>
-        /// Gets or sets the cedula.
-        /// </summary>
-        /// <value>The cedula.</value>
-     public int Cedula { get => cedula; set => cedula = value; }
-        /// <summary>
-        /// Gets or sets the nombre.
-        /// </summary>
-        /// <value>The nombre.</value>
+        public int Cedula { get => cedula; set => cedula = value; }
+
         public string Nombre { get => nombre; set => nombre = value; }
 
-        public string Sexo { get => sexo; set => sexo = value; }
+        public double Sexo { get => sexo; set => sexo = value; }
 
-        public int Telefono { get => telefono; set => telefono = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
 
-        public int Edad { get => edad; set => edad = value; }
         public override string ToString()
         {
-            return "Cedula: " + cedula + "Nombre: " + nombre + "Sexo: " + sexo + "Telefono: " + telefono + edad;
+            return "Cedula: " + cedula + "Nombre: " + nombre + "Sexo: " + sexo + "Telefono: " + telefono;
 
         }
     }
